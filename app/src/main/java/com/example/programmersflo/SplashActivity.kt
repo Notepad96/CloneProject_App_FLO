8c428733efff1b8f2a8459c12022469624627579
Notepad96 <notepad1029@gmail.com>
2021-05-11 00:59:00 +0900

    210511

--- app/src/main/java/com/example/programmersflo/SplashActivity.kt
+++ app/src/main/java/com/example/programmersflo/SplashActivity.kt
@@ -2,34 +2,35 @@ package com.example.programmersflo
 
 import android.Manifest
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.permmision_deny.view.*
 import kotlinx.android.synthetic.main.permmision_intro.view.*
+import kotlinx.android.synthetic.main.permmision_intro.view.btnPerOK
 
 class SplashActivity : AppCompatActivity() {
     val PERMISSIONCODE = 111
     private val DELAY_TIME = 1500L
-
 
     val requestPermissionms = arrayOf(
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.MANAGE_OWN_CALLS
     )
-
+    var rejectedPermissionList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        var rejectedPermissionList = ArrayList<String>()
 
         for(permission in requestPermissionms) {
             if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
@@ -38,9 +39,7 @@ class SplashActivity : AppCompatActivity() {
         }
 
         if(rejectedPermissionList.isNotEmpty()) {
-            introDialog()
-            val arr = arrayOfNulls<String>(rejectedPermissionList.size)
-            ActivityCompat.requestPermissions(this, rejectedPermissionList.toArray(arr), PERMISSIONCODE)
+            introDialog(R.layout.permmision_intro)
         } else {
             val intent = Intent(applicationContext, MainActivity::class.java)
             startActivity(intent)
@@ -49,17 +48,25 @@ class SplashActivity : AppCompatActivity() {
     }
 
 
+    fun introDialog(layout_id: Int) {
+        val myLayout = layoutInflater.inflate(layout_id, null)
 
-    fun introDialog() {
-        val myLayout = layoutInflater.inflate(R.layout.permmision_intro, null)
-        val build = AlertDialog.Builder(applicationContext).apply {
+        val build = AlertDialog.Builder(this).apply {
             setView(myLayout)
         }
         val dialog = build.create()
         dialog.show()
 
-        myLayout.btnPerOK.setOnClickListener {
-            dialog.dismiss()
+        if(layout_id == R.layout.permmision_intro) {
+            myLayout.btnPerOK.setOnClickListener {
+                dialog.dismiss()
+                val arr = arrayOfNulls<String>(rejectedPermissionList.size)
+                ActivityCompat.requestPermissions(this, rejectedPermissionList.toArray(arr), PERMISSIONCODE)
+            }
+        } else {
+            myLayout.btnPerOK.setOnClickListener {
+                finish()
+            }
         }
     }
 
@@ -71,7 +78,7 @@ class SplashActivity : AppCompatActivity() {
 
                         if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
                             Log.d("Msg", "$permission Denied")
-                            introDialog()
+                            introDialog(R.layout.permmision_deny)
                         }
                     }
                 }
