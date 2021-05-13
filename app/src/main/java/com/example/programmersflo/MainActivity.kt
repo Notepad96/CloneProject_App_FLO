531223be08bbd5bd2ef8ffe96d9bbce565d783f3
Notepad96 <notepad1029@gmail.com>
2021-05-14 00:05:59 +0900

    mainNavView

--- app/src/main/java/com/example/programmersflo/MainActivity.kt
+++ app/src/main/java/com/example/programmersflo/MainActivity.kt
@@ -3,17 +3,40 @@ package com.example.programmersflo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.exit_toast_box.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainNavigationView.setOnNavigationItemSelectedListener(this)
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_home -> {
+
+            }
+            R.id.menu_music -> {
+
+            }
+            R.id.menu_search -> {
+
+            }
+            R.id.menu_user -> {
+
+            }
+        }
+        return true
+    }
 
     var preTime: Long = 0
     override fun onBackPressed() {
