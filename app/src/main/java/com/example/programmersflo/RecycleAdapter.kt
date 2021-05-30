61d127d1fa8166c7fb53b5e7201a1ec4a7762d53
Notepad96 <notepad1029@gmail.com>
2021-05-30 23:03:55 +0900

    210530

---
+++ app/src/main/java/com/example/programmersflo/RecycleAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.programmersflo
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecycleAdapter : RecyclerView.Adapter<RecycleAdapter.MyViewHolder>() {
+    class MyViewHolder(val layout: View) : RecyclerView.ViewHolder(layout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/example/programmersflo/mainview/FirstPage.kt
+++ app/src/main/java/com/example/programmersflo/mainview/FirstPage.kt
@@ -5,12 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.programmersflo.R
+import com.example.programmersflo.RecycleAdapter
 
 class FirstPage : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        viewManager = LinearLayoutManager(activity)
+        viewAdapter = RecycleAdapter()
+
+        recyclerView = recyclerView.apply {
+
+        }
+
         return inflater.inflate(R.layout.fragment_first_page, container, false)
     }
 }
