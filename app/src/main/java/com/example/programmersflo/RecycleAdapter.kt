d1a46ff5e685c22bfdb9ad977c19913539963095
Notepad96 <notepad1029@gmail.com>
2021-05-31 20:30:39 +0900

    210531

--- app/src/main/java/com/example/programmersflo/RecycleAdapter.kt
+++ app/src/main/java/com/example/programmersflo/RecycleAdapter.kt
@@ -1,21 +1,30 @@
 package com.example.programmersflo
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item_small.view.*
 
 class RecycleAdapter : RecyclerView.Adapter<RecycleAdapter.MyViewHolder>() {
+    var imgList = listOf<Int>( R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5 )
+    var textList = listOf<String>( "\'시간\'을 사고팔 수 있다면", "시간을 파는 상점2", "미드나잇 라이브러리", "타임머신과 과학좀 하는 로봇", "가나다라마바사아자차카타파하")
+
     class MyViewHolder(val layout: View) : RecyclerView.ViewHolder(layout)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        val layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_small, parent, false)
+
+        return MyViewHolder(layout)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.layout.list_image_item.setBackgroundResource(imgList[position])
+        holder.layout.list_text_item.text = textList[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return imgList.size
     }
 }
--- app/src/main/java/com/example/programmersflo/mainview/FirstPage.kt
+++ app/src/main/java/com/example/programmersflo/mainview/FirstPage.kt
@@ -1,31 +1,41 @@
 package com.example.programmersflo.mainview
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.programmersflo.R
 import com.example.programmersflo.RecycleAdapter
+import kotlinx.android.synthetic.main.fragment_first_page.*
+import kotlinx.android.synthetic.main.fragment_first_page.view.*
 
 class FirstPage : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var viewManager: LinearLayoutManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val view  = inflater.inflate(R.layout.fragment_first_page, container, false)
 
-        viewManager = LinearLayoutManager(activity)
+        viewManager = LinearLayoutManager(requireContext())
+        viewManager.orientation = LinearLayout.HORIZONTAL
         viewAdapter = RecycleAdapter()
 
-        recyclerView = recyclerView.apply {
-
+        recyclerView = view.recycleTest.apply {
+            setHasFixedSize(true)
+            layoutManager= viewManager
+            adapter = viewAdapter
         }
 
-        return inflater.inflate(R.layout.fragment_first_page, container, false)
+        return view
     }
+
 }
